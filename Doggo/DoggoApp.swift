d62659215d06e7d4c897cdb61aa2505794314cc2
LudovicRocher01 <ludovicrocher01@yahoo.fr>
2025-05-02 00:31:55 +0200

    Initial Commit

---
+++ Doggo/DoggoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoggoApp.swift
+//  Doggo
+//
+//  Created by Ludovic Rocher on 02/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct DoggoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
